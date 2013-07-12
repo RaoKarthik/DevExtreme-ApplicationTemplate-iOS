4f8d7631eb648294861fd8bec79a06e25a3321e9
jauseg <jauseg@gmail.com>
2013-07-12 12:42:52 +0400

    codova 2.8.0

--- CordovaLib/Classes/CDVTimer.h
+++ CordovaLib/Classes/CDVTimer.h
@@ -6,9 +6,9 @@
  to you under the Apache License, Version 2.0 (the
  "License"); you may not use this file except in compliance
  with the License.  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing,
  software distributed under the License is distributed on an
  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -17,9 +17,11 @@
  under the License.
  */
 
-// accessibility label for recording button
-"toggle audio recording" = "grabación de audio cambiar";
-// notification spoken by VoiceOver when timed recording finishes
-"timed recording complete" = "tiempo de grabación completo";
-// accessibility hint for display of recorded elapsed time
-"recorded time in minutes and seconds" = "tiempo registrado en minutos y segundos";
+#import <Foundation/Foundation.h>
+
+@interface CDVTimer : NSObject
+
++ (void)start:(NSString*)name;
++ (void)stop:(NSString*)name;
+
+@end
--- CordovaLib/Classes/CDVUserAgentUtil.h
+++ CordovaLib/Classes/CDVUserAgentUtil.h
@@ -6,9 +6,9 @@
  to you under the Apache License, Version 2.0 (the
  "License"); you may not use this file except in compliance
  with the License.  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing,
  software distributed under the License is distributed on an
  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -17,10 +17,11 @@
  under the License.
  */
 
+#import <Foundation/Foundation.h>
 
-// accessibility label for recording button
-"toggle audio recording" = "börja/avsluta inspelning";
-// notification spoken by VoiceOver when timed recording finishes
-"timed recording complete" = "inspelning har avslutad";
-// accessibility hint for display of recorded elapsed time
-"recorded time in minutes and seconds" = "inspelad tid in minuter och sekund";
+@interface CDVUserAgentUtil : NSObject
++ (NSString*)originalUserAgent;
++ (void)acquireLock:(void (^)(NSInteger lockToken))block;
++ (void)releaseLock:(NSInteger*)lockToken;
++ (void)setUserAgent:(NSString*)value lockToken:(NSInteger)lockToken;
+@end
