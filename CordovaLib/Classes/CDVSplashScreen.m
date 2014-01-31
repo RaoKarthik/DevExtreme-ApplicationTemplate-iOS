ee0794b3bafff077dc06b74bb7973543dc6d7f16
jauseg <jauseg@gmail.com>
2014-01-31 13:58:59 +0400

    Added padding for UIWebView with iOS7 statusbar height.

--- CordovaLib/Classes/CDVSplashScreen.m
+++ CordovaLib/Classes/CDVSplashScreen.m
@@ -22,7 +22,6 @@
 #define kSplashScreenDurationDefault 0.25f
 #define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 
-
 @implementation CDVSplashScreen
 
 - (void)pluginInitialize
@@ -172,9 +171,11 @@
     // There's a special case when the image is the size of the screen.
     if (CGSizeEqualToSize(screenSize, imgBounds.size)) {
         CGRect statusFrame = [self.viewController.view convertRect:[UIApplication sharedApplication].statusBarFrame fromView:nil];
+        
         if (SYSTEM_VERSION_LESS_THAN(@"7.0")) {
             imgBounds.origin.y -= statusFrame.size.height;
         }
+        
     } else {
         CGRect viewBounds = self.viewController.view.bounds;
         CGFloat imgAspect = imgBounds.size.width / imgBounds.size.height;
